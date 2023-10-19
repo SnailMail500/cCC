5a038293c332af6b32bd5d04c4b0941f79381d1b
Alex Snell <alex_snell@icloud.com>
2023-10-19 14:12:12 +0100

    Add project files.

---
+++ computer coordinate conversion/Program.vb
@@ -0,0 +1,12 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        doTheThing(1, 8)
+    End Sub
+
+    Sub doTheThing(ByRef x As Integer, y As Integer)
+
+    End Sub
+End Module
